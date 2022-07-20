9ea5e30a24c88bf3fef348e554d93cf05555e50d
github_menpett <mfmendozap@gmail.com>
2022-07-20 14:20:26 +0200

    Ejercicio_18_en python sin resolución

---
+++ basico/Ejercicio_18.py
@@ -0,0 +1,36 @@
+# EJERCICIO 1
+
+"""
+    Definir una función max()
+    que tome como argumento dos números y devuelva el mayor de ellos. 
+    (Es cierto que python tiene una función max() incorporada,
+    pero hacerla nosotros mismos es un muy buen ejercicio).
+"""
+
+def max():
+    pass
+
+# EJERCICIO 2
+
+"""
+    Definir una función max_de_tres(), 
+    que tome tres números como argumentos y 
+    devuelva el mayor de ellos.
+"""
+
+def max_de_tres():
+    pass
+
+# EJERCICIO 3
+
+"""
+    Definir una función que calcule la longitud de una lista 
+    o una cadena dada. 
+    (Es cierto que python tiene la función len() incorporada, 
+    pero escribirla por nosotros mismos resulta un muy buen ejercicio.
+"""
+
+def longitud():
+    pass
+
+print("Enhorabuena acabaste los ejercicios")
