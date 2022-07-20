fa255722ab2812d1637a6d08aa1484fedbb45081
github_menpett <mfmendozap@gmail.com>
2022-07-20 14:53:49 +0200

    Ejercicio_18_resuelto_inicial

--- basico/Ejercicio_18.py
+++ basico/Ejercicio_18.py
@@ -1,14 +1,20 @@
 # EJERCICIO 1
 
 """
-    Definir una función max()
+    Definir una función maximo()
     que tome como argumento dos números y devuelva el mayor de ellos. 
     (Es cierto que python tiene una función max() incorporada,
     pero hacerla nosotros mismos es un muy buen ejercicio).
 """
 
 def max():
-    pass
+    x = int(input('Introduce un valor para la variable x:....'))
+    y = int(input('Introduce un valor para la variable y:....'))
+
+    if x > y:
+        return print(f'La variable "x" {x} es mayor que la variable "y" {y}')
+    else:
+        return print(f'La variable "x" {x} es mayor que la variable "y" {y}')
 
 # EJERCICIO 2
 
@@ -19,7 +25,17 @@ def max():
 """
 
 def max_de_tres():
-    pass
+    x = int(input('Introduce un valor para la variable x:....'))
+    y = int(input('Introduce un valor para la variable y:....'))
+    z = int(input('Introduce un valor para la variable z:....'))
+    
+
+    if ((x > y) and (x > z)) == True:
+        return print(f'La variable "x" {x} es mayor que la variable "y" {y} y que la variable "z" {z}')
+    elif ((y > x) and (y > z)) == True:
+        return print(f'La variable "y" {y} es mayor que las variables "x" {x} y "z" {z}')
+    else:        
+        return print(f'La variable "z" {z} es mayor que las variables "x" {x} e "y" {y}')
 
 # EJERCICIO 3
 
@@ -31,6 +47,13 @@ def max_de_tres():
 """
 
 def longitud():
-    pass
+    palabra = input('Introduce una palabra para conocer su longitud: ....')
+    contador = 0
+
+    for i in palabra:
+        contador += 1
+    
+    return print(f"La palabra tiene {contador} letras")
+
 
 print("Enhorabuena acabaste los ejercicios")
