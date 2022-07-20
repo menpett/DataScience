8cef1e8af9a5a5ebef06335fcea9c589e5d1dd27
github_menpett <mfmendozap@gmail.com>
2022-07-20 13:57:19 +0200

    segundo ejercicio

--- basico/Ejercicio1.py
+++ basico/Ejercicio1.py
@@ -10,3 +10,16 @@ Variable 'notas' de valor menos tres y muestre valor. 1.24
 notas = -3
 
 print('Notas: ', notas)
+
+
+# Ejercicio
+
+# Describe una variable con nombre "notas" 
+# que tenga el valor 10, y la imprima 
+
+"""
+Variable 'notas' de valor diez y muestre valor. 1.24
+"""
+notas = 10
+
+print('Notas: ', notas)
