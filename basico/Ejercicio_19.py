8f84007d6f539127c36856d42f98cdc345f242a9
github_menpett <mfmendozap@gmail.com>
2022-07-21 14:32:22 +0200

    Ejercicio_19_Enunciados

---
+++ basico/Ejercicio_19.py
@@ -0,0 +1,32 @@
+# Ejercicio 1
+
+"""
+    Definir una función inversa() que calcule la inversion de una cadena. 
+    Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse".
+"""
+
+def inversa():
+    pass
+
+
+# Ejercicio 2
+
+"""
+    Definir una funcion es_palindromo() que reconoce palindromo 
+    palabras  que tiene el mismo aspecto escritas invertidas), ejemplo: es_palindromo("radar")
+    tendría que devolver True.
+"""
+
+def es_palindromo():
+    pass
+
+# Ejercicio 3
+
+"""
+    Definir una funcion superposicion() que tome dos listas y devuelva True si tiene al
+    menos 1 elemento en común o devuelva False en caso contrario. Escribe la función usando el bucle for 
+    aninado.
+"""
+
+def superposicion():
+    pass
