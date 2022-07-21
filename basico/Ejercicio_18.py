3f81b8ce96781602481712c02fd7ec9504cfd924
github_menpett <mfmendozap@gmail.com>
2022-07-21 12:32:42 +0200

    Se incluye Ej 4 y 5. Modifica ej. 3

--- basico/Ejercicio_18.py
+++ basico/Ejercicio_18.py
@@ -7,6 +7,9 @@
     pero hacerla nosotros mismos es un muy buen ejercicio).
 """
 
+from ast import IsNot, NotIn
+
+
 def maximo():
     x = int(input('Introduce un valor para la variable x:....'))
     y = int(input('Introduce un valor para la variable y:....'))
@@ -18,7 +21,7 @@ def maximo():
     else:
         return 'La variable "x" {x} es mayor que la variable "y" {y}'
 
-maximo()
+#maximo()
 
 # EJERCICIO 2
 
@@ -29,18 +32,18 @@ maximo()
 """
 
 def max_de_tres():
-    x = int(input('Introduce un valor para la variable x:....'))
-    y = int(input('Introduce un valor para la variable y:....'))
-    z = int(input('Introduce un valor para la variable z:....'))
+    x = int(input('Ej2. Introduce un valor para la variable x:....'))
+    y = int(input('Ej2. Introduce un valor para la variable y:....'))
+    z = int(input('Ej2. Introduce un valor para la variable z:....'))
     
 
     if ((x > y) and (x > z)) == True:
-        return print(f'La variable "x" {x} es mayor que la variable "y" {y} y que la variable "z" {z}')
+        return print(f'Ej2. La variable "x" {x} es mayor que la variable "y" {y} y que la variable "z" {z}')
     elif ((y > x) and (y > z)) == True:
-        return print(f'La variable "y" {y} es mayor que las variables "x" {x} y "z" {z}')
+        return print(f'Ej2. La variable "y" {y} es mayor que las variables "x" {x} y "z" {z}')
     else:        
-        return print(f'La variable "z" {z} es mayor que las variables "x" {x} e "y" {y}')
-max_de_tres()
+        return print(f'Ej2. La variable "z" {z} es mayor que las variables "x" {x} e "y" {y}')
+#max_de_tres()
 
 # EJERCICIO 3
 
@@ -52,13 +55,52 @@ max_de_tres()
 """
 
 def longitud():
-    palabra = input('Introduce una palabra para conocer su longitud: ....')
+    palabra = input('Ej3. Introduce una palabra para conocer su longitud: ....')
     contador = 0
 
     for i in palabra:
-        contador += 1
+            contador += 1
     
-    return print(f"La palabra tiene {contador} letras")
+    return print(f"Ej3. La palabra tiene {contador} letras")
 
+#longitud()
+
+# EJERCICIO 4
+
+"""
+    Escribir una función que tome un carácter y devuelva True si es una vocal,
+    de lo contrario devuelve False.
+"""
+
+
+def es_vocal(inp):
+    if (inp) in 'AEIOUaeiou':
+        return print(f'Ej4. Retorna: {True}')
+    else:
+        return print(f'Ej4. Retorna: {False}')
+
+es_vocal('p')
+
+# EJERCICIO 5
+
+"""
+    Escribir una función suma() y una función multip() que sumen y multipliquen
+    respectivamente todos los números de una lista.
+    Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+"""
+def suma(lista):
+    sum = 0
+    for i in lista:
+        sum += i
+    return print(f'Ej5. La suma es {sum}')
+
+def multip(lista):
+    mul = 1
+    for i in lista:
+        mul *= i
+    return print(f'Ej5. La multiplicación es {mul}')
+
+suma([1,2,3,4])
+multip([1,2,3,4])
 
 print("Enhorabuena acabaste los ejercicios")
