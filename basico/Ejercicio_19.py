97f21facda847ffcf8bc8f9152d200a8f8fbd0ce
github_menpett <mfmendozap@gmail.com>
2022-07-21 15:23:15 +0200

    Ejercicio_19_Resuelto

--- basico/Ejercicio_19.py
+++ basico/Ejercicio_19.py
@@ -5,8 +5,11 @@
     Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse".
 """
 
-def inversa():
-    pass
+def inversa(cadena):
+    cadena = str(cadena)
+    print(cadena[::-1])
+
+inversa('estoy probando')
 
 
 # Ejercicio 2
@@ -17,16 +20,31 @@ def inversa():
     tendría que devolver True.
 """
 
-def es_palindromo():
-    pass
+def es_palindromo(cadena):
+    cadena1 = cadena[::-1]
+    # print(cadena,cadena1)
+    if cadena == cadena1:
+        return print(f'La palabra {cadena} es palindromo')
+    else:
+        return print(f'La palabra {cadena} no es palindromo')
+
+es_palindromo('radar')
 
 # Ejercicio 3
 
 """
     Definir una funcion superposicion() que tome dos listas y devuelva True si tiene al
-    menos 1 elemento en común o devuelva False en caso contrario. Escribe la función usando el bucle for 
+    menos  o devuelva False en caso contrario. Escribe la función usando el bucle for 
     aninado.
 """
 
-def superposicion():
-    pass
+def superposicion(lista1, lista2):
+    for i in lista1:
+        print(i)
+        if i in lista2:
+            return print(f'El carácter "{i}" es común en ambas listas')
+        
+
+    return print(f'Las listas no tienen carácteres comunes')
+
+superposicion([98,8,6,'a','mi',10], ['lista2','mi',2,10,'mi'])
