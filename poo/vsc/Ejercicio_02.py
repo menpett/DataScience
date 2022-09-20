0e5312ca8ce766742b9bade505d766f4ab28cbda
menpett <mfmendozap@gmail.com>
2022-09-20 13:31:53 +0200

    Enunciados

---
+++ poo/vsc/Ejercicio_02.py
@@ -0,0 +1,53 @@
+# EJERCICIO 1
+"""
+    Crea una clase llamada Persona. Sus atributos son: nombre, edad y DNI. Construye los siguientes métodos para la clase:
+
+    Un constructor, donde los datos pueden estar vacíos.
+    mostrar(): Muestra los datos de la persona.
+    esMayorDeEdad(): Devuelve un valor indicando si es mayor de edad.
+"""
+
+# EJERCICIO 2
+"""
+    Crea una clase llamada Cuenta que tendrá los siguientes atributos: titular (que es una persona) y cantidad (puede tener decimales).
+    Construye los siguientes métodos para la clase:
+
+    Un constructor, donde los datos pueden estar vacíos.
+    El atributo no se puede modificar directamente, sólo ingresando o retirando dinero.
+    mostrar(): Muestra los datos de la cuenta.
+    ingresar(cantidad): se ingresa una cantidad a la cuenta, si la cantidad introducida es negativa, no se hará nada.
+    retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en números rojos si es saldo negativo.
+"""
+
+# 1) Ingresar en positivo
+
+# 2) Ingresar negativo
+
+# 3) Retirar dinero
+
+# 4) Retirar dinero en número rojos
+
+
+# EJERCICIO 3
+
+"""
+    Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase CuantaJoven que deriva de la anterior.
+    Cuando se crea esta nueva clase, además del titular y la cantidad se
+    debe guardar una bonificación que estará expresada en tanto por ciento.
+    Construye los siguientes métodos para la clase:
+
+    Un constructor.
+    En esta ocasión los titulares de este tipo de cuenta tienen que ser mayor de edad,
+    por lo tanto hay que crear un método esTitularValido() que devuelve verdadero
+    si el titular es mayor de edad pero menor de 25 años y falso en caso contrario.
+
+    Además la retirada de dinero sólo se podrá hacer si el titular es válido.
+
+    El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la cuenta.
+    Piensa los métodos heredados de la clase madre que hay que reescribir.
+"""
+
+# 1) Mayor de edad
+
+# 2) Menor de edad
+
